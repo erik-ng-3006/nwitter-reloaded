9b729b160f5ba100f7ce9c9f3a5739f7ef877818
erik-ng-3006 <erikng3006@gmail.com>
2025-02-21 23:24:50 +0700

    setup routing and global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,56 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/layout';
+import Home from './routers/home';
+import Profile from './routers/profile';
+import Login from './routers/login';
+import CreateAccount from './routers/create-account';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				path: '',
+				element: <Home />,
+			},
+			{
+				path: 'profile',
+				element: <Profile />,
+			},
+		],
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '/create-account',
+		element: <CreateAccount />,
+	},
+]);
+
+const GlobalStyles = createGlobalStyle`
+${reset};
+* {
+  box-sizing: border-box;
+}
+body {
+  background-color: black;
+  color: white;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+}
+`;
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<>
+			<GlobalStyles />
+			<RouterProvider router={router} />
+		</>
+	);
 };
 
 export default App;
